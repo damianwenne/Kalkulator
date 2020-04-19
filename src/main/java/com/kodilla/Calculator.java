e57af75c2f9b383bd2e032a6f5f564046ae7130d
damianwenne <damianwenne@gmail.com>
2020-04-19 19:36:01 +0200

    Zadanie 4.6

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public void addition() {
+
+    }
+
+    public void subtraction() {
+
+    }
+
+    public static void main(String[] args);
+
+
+}
